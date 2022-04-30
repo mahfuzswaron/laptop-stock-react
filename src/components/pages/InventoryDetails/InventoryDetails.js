5255370d8dacfc517695b095e7d8f07e0aa7cb27
mahfuzswaron <mahfuzswaron@gmail.com>
2022-04-30 23:29:13 +0600

    delevery button functionality done

--- src/components/pages/InventoryDetails/InventoryDetails.js
+++ src/components/pages/InventoryDetails/InventoryDetails.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import useInventory from '../../../hooks/useInventory';
 
@@ -7,14 +7,24 @@ const InventoryDetails = () => {
 
     const {id} =  useParams();
     const [inventory] = useInventory(id);
+    const {name, description, price, supplier, quantity, img} = inventory;
+    const invId = "don't forget to add id here";
+    let quantt = 0;
+    const [invQuantity, setInvQuantity] = useState(quantt);
+    
+    useEffect(()=>{
+        setInvQuantity(quantity)
+    }, [quantity])
 
     if(!inventory){
         return <p>loading...</p>
     }
     
+    if(!invQuantity){
+        return <p>quantity loading...</p> 
+    }
     
-    const {name, description, price, supplier, quantity, img} = inventory;
-    const invId = "don't forget to add id here";
+
     return (
         <div className='flex bg-white border hover:drop-shadow-xl p-3 m-5'>
 
@@ -27,11 +37,11 @@ const InventoryDetails = () => {
         <p className='mt-1'>Product id: {invId}</p>
         <p className='mt-1'>{description}</p>
         <p className='mt-1'><strong>${price}</strong></p>
-        <p className='mt-1'>{quantity} available </p>
+        <p className='mt-1'>{invQuantity} available </p>
         <p className='mt-1'>Supplier: {supplier}</p>
         <button 
         className='bg-blue-400 hover:bg-blue-500 px-3 py-2 mt-2 rounded text-white' 
-        onClick={() => console.log('clicked')}
+        onClick={() => setInvQuantity(invQuantity - 1)}
         >Delevered</button>
         </div>
         </div>
