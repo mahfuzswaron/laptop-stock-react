829c438e218ec37624a461c5437bfbf277b82cc2
mahfuzswaron <mahfuzswaron@gmail.com>
2022-05-08 18:33:42 +0600

    Sale Analytics chart added

--- src/components/pages/Home/Home .js
+++ src/components/pages/Home/Home .js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Bar, BarChart, Legend, ResponsiveContainer, Tooltip } from 'recharts';
 import UseInventories from '../../../hooks/UseInventories';
 import Calculator from '../Calculator/Calculator';
 import Inventory from '../Inventory/Inventory';
@@ -9,7 +10,7 @@ const Home = () => {
     const inventories6 = inventories.slice(0, 6);
     return (
         <div>
-            <div className='flex items-start'>
+            <section className='flex items-start'>
                 <div className='mt-10 ml-5 w-1/2'>
                     <h1 className='text-6xl font-semibold text-gray-500 '>Welcome to <span className='block mt-1 text-7xl text-blue-500'>Laptop Stock</span></h1>
                     <p className='mt-5'>
@@ -20,8 +21,7 @@ const Home = () => {
                 <div className=' right-5 w-1/2'>
                      <img className='w-3/4 relative left-16 bottom-10' src='https://i.ibb.co/WtkBh27/HTB1v4-XLa-KT2g-K0j-SZFvq6xn-FXXa-C.jpg' alt='laptop' />
                 </div>
-
-            </div>
+            </section>
 
             {/* inventories section starts */}
             <section >
@@ -40,15 +40,29 @@ const Home = () => {
                     <div className='flex justify-center space-x-4 mx-auto'>
                         <button className='w-48 py-2 px-5 hover:bg-blue-700 text-white bg-blue-500 rounded'><Link to={'/inventories'}>See All</Link></button>
                         <button className='w-48 py-2 px-5 hover:bg-blue-700 text-white bg-blue-500 rounded'><Link to={'/manageinventories'}>Manage Inventories</Link></button>
-                        
-                </div>            
-}
+                    </div> 
+                    
+                }
             </section>
             {/* -------------------- */}
 
             {/* Calculator section starts  */}
             <section>
                 <Calculator></Calculator>
+            </section>
+
+            {/* graph chart section starts */}
+            <section className=''>
+                <h3 className='text-3xl text-blue-500 font-semibold text-center my-10'>Sale Analytics</h3>
+                <div className='flex justify-center mx-auto '>
+                    <BarChart className='' width={800} height={400} data={inventories}>
+                    <Bar dataKey="sold" fill="#8884d8" />
+                    <Tooltip></Tooltip>
+                    <Legend></Legend>
+                    </BarChart>
+                </div>
+                
+                
             </section>
         </div>
     );
