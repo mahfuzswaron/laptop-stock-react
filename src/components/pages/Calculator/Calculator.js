3c6b4bb106ee1f4b0bdf3def51cfc8865112f517
mahfuzswaron <mahfuzswaron@gmail.com>
2022-05-08 17:23:25 +0600

    calculator section done

--- src/components/pages/Calculator/Calculator.js
+++ src/components/pages/Calculator/Calculator.js
@@ -2,6 +2,14 @@ import React, { useState } from 'react';
 
 const Calculator = () => {
     const [total, setTotal] = useState(0);
+    const calculate = (e) => {
+        e.preventDefault();
+        const quantity = parseInt(e.target.quantity.value);
+        const price = parseFloat(e.target.price.value);
+        const result = quantity * price;
+        setTotal(result);
+        e.target.reset();
+    }
 
     return (
         <div className='w-1/3 mx-auto'>
@@ -9,10 +17,10 @@ const Calculator = () => {
             <div className='display w-full h-24 border flex justify-center items-center text-2xl'>
                 <p>${total}</p>
             </div>
-            <form className=''>
-                <div className=''>
-                    <input className='w-1/2 p-3 border my-1' type={'number'} name='quantity' placeholder='quantity' />
-                    <input className='w-1/2 p-3 border my-1' type={'number'} name='price' placeholder='price' />
+            <form onSubmit={e => calculate(e)}>
+                <div>
+                    <input required className='w-1/2 p-3 border my-1' type={'number'} name='quantity' placeholder='quantity' />
+                    <input required className='w-1/2 p-3 border my-1' type={'number'} name='price' placeholder='price' />
                 </div>
                 <button className='bg-green-500 hover:bg-green-600 px-5 py-3 w-full text-white font-bold text-3xl' type='submit' >=</button>
             </form>
