a734bd185b44a0a500909fbd804b681a323d2583
mahfuzswaron <mahfuzswaron@gmail.com>
2022-05-08 10:26:04 +0600

    Blogs Section done

--- src/components/pages/Blogs/Blogs.js
+++ src/components/pages/Blogs/Blogs.js
@@ -5,7 +5,7 @@ const Blogs = () => {
     const qnas = [
         {
             question: "What are the difference between Javascript and Nodejs?",
-            answer: "a. Javascript is a programming language. NodeJs is a javascript runtime envirnment.\n b. Js is is basically used in frontend development. NodeJs is used in backend development.\n c. Js can run only in browser. To run Js outside of browser, we use NodeJs."
+            answer: "> Javascript is a programming language. NodeJs is a javascript runtime envirnment.\n > Js is is basically used in frontend development. NodeJs is used in backend development.\n > Js can run only in browser. To run Js outside of browser, we use NodeJs."
         },
         {
             question: "When to use Node and When to use MongoDB?",
@@ -13,13 +13,14 @@ const Blogs = () => {
         },
         {
             question: "What are the difference between SQL and NoSQL?",
-            answer: "a. SQL are defined as query language. NoSQL are not a language. \n b. SQL are relational database management system (RDBMS). NoSQL are distributed database management system. \n c. SQL are suitable for complex data storage. NoSQL are for hierachical data storage"
+            answer: "> SQL are defined as query language. NoSQL are not a language. \n > SQL are relational database management system (RDBMS). NoSQL are distributed database management system. \n > SQL are suitable for complex data storage. NoSQL are for hierachical data storage"
         },
         {
             question: "What is the purpose of JWT and how does it works?",
-            answer: "This is the difference between them"
+            answer: "The purpose of JWT is to authenticate a user and share information. \n It has a part - signature. it contains a specific information about the user and the server varifies the information to authorize the user."
         }
-    ]
+    ];
+
     return (
         <div>
             <h1 className='text-4xl text-blue-500 font-semibold text-center my-10'>Blogs</h1>
--- src/components/pages/Blogs/Qna.js
+++ src/components/pages/Blogs/Qna.js
@@ -3,9 +3,9 @@ import React from 'react';
 const Qna = ({ qna }) => {
     const {question, answer} = qna
     return (
-        <div className='p-3 m-5 rounded  border-l-4 border-blue-500'>
-            <h4 className='text-2xl '>{question}</h4>
-            <p className='text-xl whitespace-pre-line'>{ answer}</p>
+        <div className='px-3  m-5 rounded  border-l-4 border-blue-500'>
+            <h4 className='text-2xl font-semibold '>{question}</h4>
+            <p className='text-lg whitespace-pre-line'>{ answer}</p>
         </div>
     );
 };
