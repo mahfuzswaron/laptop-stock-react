5d432504f557992e396166cb2f9bbde96cd39d2f
mahfuzswaron <mahfuzswaron@gmail.com>
2022-06-03 23:45:16 +0600

    login done

--- src/components/pages/Login/Login.js
+++ src/components/pages/Login/Login.js
@@ -13,25 +13,25 @@ const override = css`
 `;
 const Login = () => {
     const [
-  signInWithEmailAndPassword,
-  user,
-  loading,
-  error,
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        error,
     ] = useSignInWithEmailAndPassword(auth);
-let navigate = useNavigate();
-let location = useLocation();
-let from = location.state?.from?.pathname || "/";
+    let navigate = useNavigate();
+    let location = useLocation();
+    let from = location.state?.from?.pathname || "/";
 
-    
-    const handleSubmit =async e => {
+
+    const handleSubmit = async e => {
         e.preventDefault();
 
         const email = e.target.email.value;
         const password = e.target.password.value;
         await signInWithEmailAndPassword(email, password);
-        
+
     }
-    
+
     if (loading) {
         return <div className='flex justify-center items-center'>
             <BeatLoader loading={loading} color={'blue'} style={override} size={10}></BeatLoader>
@@ -42,23 +42,27 @@ let from = location.state?.from?.pathname || "/";
     }
     return (
         <div>
-             <h3 className='text-center text-blue-500 text-3xl font-semibold my-5'>Please Login</h3>
+            <h3 className='text-center text-primary text-4xl font-semibold my-5'>Please Login</h3>
             <div className='mx-auto w-1/2'>
                 <form onSubmit={(e) => handleSubmit(e)} className='flex flex-col space-y-4'>
-                <input required className='border p-2' type={'email'} name={'email'} placeholder='Email Address' />
-                <input required className='border p-2' type={'password'} name='password' placeholder='Your Password' />
-                <button className='p-2 bg-blue-500 text-white hover:bg-blue-600 rounded' type={'submit'}>Login</button>
-                {
-                  error && <p className='text-red-500'>{ error.message}</p>
-                }
+                    <input required className='border p-2 rounded-lg' type={'email'} name={'email'} placeholder='Email Address' />
+                    <input required className='border p-2 rounded-lg' type={'password'} name='password' placeholder='Your Password' />
+                    <button className='p-2 btn btn-primary' type={'submit'}>Login</button>
+                    {
+                        error && <p className='text-red-500'>{error.message}</p>
+                    }
                 </form>
-                <p>Don't Have an account? <Link className='hover:text-blue-500 underline' to={'/registar'}>Registar here</Link></p>
+                <p className=' mt-2'>Don't Have an account? <Link className='hover:text-primary underline' to={'/registar'}>Registar here</Link></p>
             </div>
+
+            {/* or div */}
             <div className='flex items-center justify-center'>
                 <hr className='w-1/3'></hr>
                 <p className='text-xl m-5'>Or</p>
                 <hr className='w-1/3'></hr>
             </div>
+            {/* ---- */}
+
             <SocialLogin></SocialLogin>
         </div>
     );
