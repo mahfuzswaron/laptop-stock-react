9eaf3b18b6a44ec0f46e004f47a41e5109413282
mahfuzswaron <mahfuzswaron@gmail.com>
2022-06-04 10:30:58 +0600

    banner responsiveness done

--- src/components/pages/Home/Home .js
+++ src/components/pages/Home/Home .js
@@ -21,15 +21,15 @@ const Home = () => {
     }
     return (
         <div>
-            <section className='flex items-center bg-[#EFEFEF]'>
-                <div className='flex items-center mx-20'>
+            <section className='flex items-center bg-[#EFEFEF] py-8'>
+                <div className='flex items-center lg:mx-20 mx-10'>
                     <div className='w-1/2 h-1/2'>
-                        <h1 className='text-6xl font-semibold text-seconary '>Welcome to <span className='block text-7xl text-primary'>Laptop Stock</span></h1>
-                        <p className='mt-5'>
+                        <h1 className='lg:text-6xl sm:text-3xl font-semibold text-seconary '>Welcome to <span className='block lg:text-7xl sm:text-4xl text-primary'>Laptop Stock</span></h1>
+                        <p className='lg:mt-5 my-2 text-[9px] lg:text-lg'>
                             Laptop Stock is a place where the world's best Laptops are stored. Browse the app to add yours and manage them.
                             Have a nice day.
                         </p>
-                        <button className='btn btn-primary btn-outline mt-3'><Link to={'/inventories'}>Let's Start <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 inline" viewBox="0 0 20 20" fill="currentColor">
+                        <button className='btn btn-sm md:btn-md lg:btn-lg btn-primary btn-outline text-xs lg:text-lg" '><Link to={'/inventories'}>Let's Start <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 inline" viewBox="0 0 20 20" fill="currentColor">
                             <path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd" />
                         </svg></Link></button>
 
@@ -69,9 +69,9 @@ const Home = () => {
             </section>
 
             {/* graph chart section starts */}
-            <section className='mt-20 mb-10 w-screen lg:h-[500px] sm:h-[250px]'>
+            <section className='mt-20 mb-10 w-full lg:h-[500px] sm:h-[250px]'>
                 <h3 className='text-4xl text-primary font-semibold text-center mb-5'>Sale Analytics Chart</h3>
-                <div className='flex justify-center mx-auto w-full h-4/5'>
+                <div className='flex justify-center mx-auto lg:w-full sm:w-full h-4/5'>
                     <ResponsiveContainer width="90%" height="100%">
                         <BarChart data={inventories}>
                             <Bar dataKey="sold" stackId="a" fill="#0490f4" />
